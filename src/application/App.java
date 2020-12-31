b7ada035e49aeaac23c915f5981581ec6c8f2d66
Rodrigo Torres <rodrigo_fbm@hotmail.com>
2020-12-31 19:10:17 -0300

    implements findById

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,23 @@
 package application;
 
-import java.sql.Connection;
-
 import db.DB;
+import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDAO;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Connection conn = DB.getConnection();
-
-        conn.close();
+        SellerDAO sellerDAO = DaoFactory.createSellerDAO();
+        
+        try {
+        	Seller seller = sellerDAO.findById(8);
+            
+            System.out.println(seller);
+            
+            DB.closeConnection();
+		} catch (DbException e) {
+			e.printStackTrace();
+		}
     }
 }
